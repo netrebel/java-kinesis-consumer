ee3adae5e61fcb697a4d74b7cce22b973cb01859
Miguel Reyes <mreyes@life360.com>
2023-01-30 22:03:13 -0800

    initial commit

---
+++ src/main/java/com/example/javakinesisconsumer/JavaKinesisConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javakinesisconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKinesisConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKinesisConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javakinesisconsumer/JavaKinesisConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javakinesisconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKinesisConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
