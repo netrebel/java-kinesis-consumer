09ebc3c600671e9d61d116ebecb815a783576c1c
Miguel Reyes <mreyes@life360.com>
2023-02-01 12:58:33 -0800

    Removed DynamoDB Client custom config

--- src/main/java/com/example/javakinesisconsumer/config/DynamoDbConfiguration.java
+++ src/main/java/com/example/javakinesisconsumer/config/DynamoDbConfiguration.java
@@ -2,13 +2,10 @@ package com.example.javakinesisconsumer.config;
 
 import static com.amazonaws.SDKGlobalConfiguration.AWS_ROLE_ARN_ENV_VAR;
 
-import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.retry.PredefinedRetryPolicies;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.example.javakinesisconsumer.config.DynamoDbProperties.Client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -31,49 +28,10 @@ public class DynamoDbConfiguration {
         this.dbProperties = dbProperties;
     }
 
-    /**
-     * Override this bean to customize settings.
-     * <p>
-     * Settings for back-off and retries
-     * <p/>
-     * {@link PredefinedRetryPolicies#DEFAULT_RETRY_CONDITION}
-     * <p/>
-     * {@link PredefinedRetryPolicies#DYNAMODB_DEFAULT_BACKOFF_STRATEGY}
-     *
-     * <ul>
-     *   <li>Never retry on requests with non-repeatable content;
-     *   <li>Retry on client exceptions caused by IOException;
-     *   <li>Retry on service exceptions that are either 500 internal server
-     *       errors, 503 service unavailable errors, service throttling errors or
-     *       clock skew errors.
-     * </ul>
-     *
-     * @return dynamodb client configuration object
-     */
-    @Bean
-    @ConditionalOnMissingBean(name = "dynamoDbClientConfiguration")
-    ClientConfiguration dynamoDbClientConfiguration() {
-
-        Client clientProperties = dbProperties.getClient();
-        ClientConfiguration clientConfig = new ClientConfiguration()
-              .withConnectionTimeout((int) clientProperties.getConnectionTimeout().toMillis())
-              .withClientExecutionTimeout((int) clientProperties.getExecutionTimeout().toMillis())
-              .withRequestTimeout((int) clientProperties.getRequestTimeout().toMillis())
-              .withSocketTimeout((int) clientProperties.getSocketTimeout().toMillis());
-
-        if (clientProperties.useCustomRetryPolicy()) {
-            clientConfig.withRetryPolicy(
-                  PredefinedRetryPolicies
-                        .getDynamoDBDefaultRetryPolicyWithCustomMaxRetries(
-                              clientProperties.getMaxErrorRetries()));
-        }
-        return clientConfig;
-    }
-
     @Bean
     @Primary
     @ConditionalOnMissingBean(name = "amazonDynamoDbConfig")
-    AmazonDynamoDB amazonDynamoDbConfig(ClientConfiguration clientConfiguration, CredProviderDevelopment localCredProvider) {
+    AmazonDynamoDB amazonDynamoDbConfig(CredProviderDevelopment localCredProvider) {
 
         AWSCredentialsProvider awsCredentialsProvider = localCredProvider.credProvider();
         AmazonDynamoDB amazonDynamoDb =
@@ -82,7 +40,6 @@ public class DynamoDbConfiguration {
                                                new AwsClientBuilder.EndpointConfiguration(
                                                      dbProperties.getEndpoint(),
                                                      dbProperties.getRegion()))
-                                         .withClientConfiguration(clientConfiguration)
                                          .withCredentials(awsCredentialsProvider)
                                          .build();
 
